03ae5c3c8a000ae6402653450c6f5c0aa9f0d98b
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-10-23 04:49:50 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,42 +1,47 @@
-<div class="row">
-  <div class="col">
-<h1>Courses</h1>
+<head>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap Icons CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="row">
+    <div class="col">
+      <h1>Courses</h1>
+    </div>
+    <div class="col-auto">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+      </svg>
+    </div>
   </div>
-  <div class="col-auto">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-    </svg>
+
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Number</th>
+          <th>Description</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php while ($course = $courses->fetch_assoc()) { ?>
+        <tr>
+          <td><?php echo $course['course_id']; ?></td>
+          <td><?php echo $course['course_number']; ?></td>
+          <td><?php echo $course['course_description']; ?></td>
+          <td>
+            <form method="post" action="sections-by-course.php">
+              <input type="hidden" name="cid" value="<?php echo $course['course_id']; ?>">
+              <button type="submit" class="btn btn-primary">Sections</button>
+            </form>
+          </td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
-</div>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Number</th>
-        <th>Description</th>
-         <th></th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while ($course = $courses->fetch_assoc()){
-?>
-  <tr>
-    <td><?php echo $course['course_id']; ?></td>
-    <td><?php echo $course['course_number']; ?></td>
-    <td><?php echo $course['course_description']; ?></td>
-    <td>
-      <form method="post" action="sections-by-course.php">
-        <input type="hidden" name="cid" value="<?php echo $course['course_id']; ?>">
-        <button type="submit" class="btn btn-primary">Sections</button>
-      </form>
-    </td>
-  </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
-</div>
+</body>
