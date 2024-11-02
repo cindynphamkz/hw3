ace24eef38790e233cf5002e94b2fb83ffb89f9a
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 01:18:58 -0500

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -6,7 +6,7 @@ function selectBooksByAuthor($authorId) {
     if (!$conn) {
         die("Database connection failed.");
     }
-    $stmt = $conn->prepare("SELECT bookid, title, genre FROM 'Books' WHERE authorid = ?");
+    $stmt = $conn->prepare("SELECT bookid, title, genre FROM Books WHERE authorid = ?");
     $stmt->bind_param("i", $authorId);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -19,7 +19,7 @@ function selectBooks() {
     if (!$conn) {
         die("Database connection failed.");
     }
-    $stmt = $conn->prepare("SELECT bookid, authorid, title, genre FROM `Books`;");
+    $stmt = $conn->prepare("SELECT bookid, authorid, title, genre FROM Books;");
     $stmt->execute();
     $result = $stmt->get_result();
     $conn->close();
