e2980f18532d19afc8cf27d7493efe52cde170b3
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-01 23:20:02 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,11 @@
 <?php
-// db.php
-$host = '138.197.17.168'; // Replace with 'localhost' if it's hosted locally
-$dbname = 'cindypha_hw3'; // Database name as provided
-$username = 'cindypha'; // Your username
-$password = 'YES'; // Replace with your actual password
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Could not connect to the database $dbname :" . $e->getMessage());
+function get_db_connection() {
+    // Create connection
+    $conn = new mysqli('138.197.17.168', 'cindypha_hw3user', 'mI$04K.pv3-i', 'cindypha_hw3');
+    // Check connection
+    if ($conn->connect_error) {
+        return false;
+    }
+    return $conn;
 }
 ?>
