7d0be673fd96a5da194152dd586fdc6adf9c2abf
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 04:29:20 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -3,12 +3,28 @@ require_once("model-books.php");
 $pageTitle = "Books by Author";
 include "view-header.php";
 
+// Get the author ID from the URL parameter
 $authorId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
-$authorName = "Author Name";
+// Fetch the books for this author
 $books = selectBooksByAuthor($authorId);
 
-include "view-books-by-author.php";
+echo "<h1>Books by Author</h1>";
+if ($books && $books->num_rows > 0) {
+    echo "<table class='table'>";
+    echo "<thead><tr><th>Title</th><th>Genre</th></tr></thead><tbody>";
+
+    while ($book = $books->fetch_assoc()) {
+        $title = htmlspecialchars($book['Title']);
+        $genre = htmlspecialchars($book['Genre']);
+
+        echo "<tr><td>$title</td><td>$genre</td></tr>";
+    }
+
+    echo "</tbody></table>";
+} else {
+    echo "<p>No books found for this author.</p>";
+}
 
 include "view-footer.php";
 ?>
