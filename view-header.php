e16a77e2fd9a7565c57612e835e096ce4c64b647
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 01:22:28 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,4 +1,3 @@
-<!-- view-header.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">My Website</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="authors.php">Authors</a></li>
-            <li class="nav-item"><a class="nav-link" href="books.php">Books</a></li>
-        </ul>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">My Website</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="authors.php">Authors</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="books.php">Books</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="books-by-author.php">Books by Author</a> <!-- New Tab -->
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
-</nav>
