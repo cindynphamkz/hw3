385d4a2054e8119cee1eaaae9b980ee78a9fd307
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 01:26:10 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -3,16 +3,20 @@ require_once("model-books.php");
 $pageTitle = "Books by Author";
 include "view-header.php";
 
-// Assume author ID is passed as a query parameter, e.g., books-by-author.php?id=1
 $authorId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 $books = selectBooksByAuthor($authorId);
 
-if ($books->num_rows > 0) {
+if ($books && $books->num_rows > 0) {
     echo "<h1>Books by Author</h1>";
     echo "<table class='table'>";
     echo "<thead><tr><th>Title</th><th>Genre</th></tr></thead><tbody>";
+
     while ($book = $books->fetch_assoc()) {
-        echo "<tr><td>" . htmlspecialchars($book['Title']) . "</td><td>" . htmlspecialchars($book['Genre']) . "</td></tr>";
+        // Use the correct lowercase keys
+        $title = isset($book['title']) ? htmlspecialchars($book['title']) : 'N/A';
+        $genre = isset($book['genre']) ? htmlspecialchars($book['genre']) : 'N/A';
+
+        echo "<tr><td>" . $title . "</td><td>" . $genre . "</td></tr>";
     }
     echo "</tbody></table>";
 } else {
