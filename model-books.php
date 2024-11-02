78ce4179b3a63e182d13aaa07d8578070f24b18d
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 03:12:19 -0500

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -1,28 +1,33 @@
 <?php
 require_once("db.php");
 
-function selectBooksByAuthor($authorId) {
+function selectBooksByGenre($genre) {
     $conn = get_db_connection();
     if (!$conn) {
         die("Database connection failed.");
     }
-    $stmt = $conn->prepare("SELECT bookid, title, genre FROM Books WHERE authorid = ?");
-    $stmt->bind_param("i", $authorId);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $conn->close();
-    return $result;
-}
 
-function selectBooks() {
-    $conn = get_db_connection();
-    if (!$conn) {
-        die("Database connection failed.");
+    // Check if a specific genre was selected
+    if ($genre) {
+        $stmt = $conn->prepare("SELECT BookID, AuthorID, Title, Genre FROM Books WHERE genre = ?");
+        $stmt->bind_param("s", $genre);
+    } else {
+        $stmt = $conn->prepare("SELECT BookID, AuthorID, Title, Genre FROM Books");
     }
-    $stmt = $conn->prepare("SELECT bookid, authorid, title, genre FROM Books;");
+
+    if (!$stmt) {
+        die("Query preparation failed: " . $conn->error);
+    }
+
     $stmt->execute();
     $result = $stmt->get_result();
+    if (!$result) {
+        die("Getting result set failed: " . $stmt->error);
+    }
+
+    $stmt->close();
     $conn->close();
+
     return $result;
 }
 ?>
