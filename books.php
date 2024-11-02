d5aa851b4bf4012a99f18a5b8a87c193a685373e
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-01 23:22:17 -0500

    Create books.php

---
+++ books.php
@@ -0,0 +1,31 @@
+<?php
+require_once("model-books.php");
+$pageTitle = "Books";
+include "view-header.php";
+
+$books = selectBooks();
+?>
+
+<h1>All Books</h1>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Genre</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($book = $books->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $book['book_id']; ?></td>
+                <td><?php echo htmlspecialchars($book['title']); ?></td>
+                <td><?php echo htmlspecialchars($book['genre']); ?></td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</div>
+
+<?php include "view-footer.php"; ?>
