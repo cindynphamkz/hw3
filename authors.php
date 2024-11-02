25d915135426178f2a647b3b07b7e4c00f2ee22f
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 03:06:34 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -3,31 +3,30 @@ require_once("model-authors.php");
 $pageTitle = "Authors";
 include "view-header.php";
 
+// Retrieve authors
 $authors = selectAuthors();
+
+if ($authors && $authors->num_rows > 0) {
+    echo "<h1>Authors</h1>";
+    echo "<table class='table'>";
+    echo "<thead><tr><th>Name</th><th>Bio</th><th>Books</th></tr></thead><tbody>";
+
+    while ($author = $authors->fetch_assoc()) {
+        $authorId = $author['authorid'];
+        $authorName = htmlspecialchars($author['name']);
+        $bio = htmlspecialchars($author['bio']);
+
+        echo "<tr>";
+        echo "<td>$authorName</td>";
+        echo "<td>$bio</td>";
+        echo "<td><a href='books-by-author.php?id=$authorId'>View Books</a></td>";
+        echo "</tr>";
+    }
+
+    echo "</tbody></table>";
+} else {
+    echo "<p>No authors found.</p>";
+}
+
+include "view-footer.php";
 ?>
-
-<h1>Authors</h1>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Bio</th>
-                <th>Books</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($author = $authors->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $author['authorid']; ?></td>
-                <td><?php echo htmlspecialchars($author['name']); ?></td>
-                <td><?php echo htmlspecialchars($author['bio']); ?></td>
-                <td><a href="books-by-author.php?id=<?php echo $author['authorid']; ?>">View Books</a></td>
-            </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-</div>
-
-<?php include "view-footer.php"; ?>
