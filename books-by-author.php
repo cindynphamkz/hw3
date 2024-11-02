bef47b6740b126f3cbd6a45fe258ba885f5ebbce
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 03:20:46 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -3,14 +3,11 @@ require_once("model-books.php");
 $pageTitle = "Books by Author";
 include "view-header.php";
 
-// Retrieve the author ID from the query parameter
 $authorId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
-// Placeholder author name; replace this with a query to fetch the actual author name if available
-$authorName = "Author Name"; // Optional: Retrieve actual name if possible
+$authorName = "Author Name";
 $books = selectBooksByAuthor($authorId);
 
-// Include the view file for displaying books by author
 include "view-books-by-author.php";
 
 include "view-footer.php";
