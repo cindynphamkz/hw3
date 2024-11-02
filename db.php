42d368cd0dfc510de45b6f58147a74408f28b497
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-01 20:58:47 -0500

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $host = '138.197.17.168'; // Replace with 'localhost' if it's hosted locally
 $dbname = 'cindypha_hw3'; // Database name as provided
 $username = 'cindypha'; // Your username
-$password = 'Otis649!'; // Replace with your actual password
+$password = 'YES'; // Replace with your actual password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
