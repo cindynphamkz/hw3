374c7d7c49d7d163c3dac3b45bb4e43c86330aa7
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 01:01:59 -0500

    Update model-authors.php

--- model-authors.php
+++ model-authors.php
@@ -10,7 +10,7 @@ function selectAuthors() {
     }
 
     // Prepare the SQL query
-    $stmt = $conn->prepare("SELECT author_id, name, bio FROM 'authors'");
+    $stmt = $conn->prepare("SELECT AuthorID, Name, Bio FROM 'Authors'");
     if (!$stmt) {
         die("Query preparation failed: " . $conn->error);
     }
