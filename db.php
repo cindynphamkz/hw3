025b9e859631a770feebf3ab970fac455959ef46
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-01 20:56:56 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+// db.php
+$host = '138.197.17.168'; // Replace with 'localhost' if it's hosted locally
+$dbname = 'cindypha_hw3'; // Database name as provided
+$username = 'cindypha'; // Your username
+$password = 'Otis649kazuha'; // Replace with your actual password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
