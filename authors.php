284f3037f11ffc0d24a262d78528e6e6de36c1d3
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-01 23:21:26 -0500

    Create authors.php

---
+++ authors.php
@@ -0,0 +1,33 @@
+<?php
+require_once("model-authors.php");
+$pageTitle = "Authors";
+include "view-header.php";
+
+$authors = selectAuthors();
+?>
+
+<h1>Authors</h1>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Bio</th>
+                <th>Books</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($author = $authors->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $author['author_id']; ?></td>
+                <td><?php echo htmlspecialchars($author['name']); ?></td>
+                <td><?php echo htmlspecialchars($author['bio']); ?></td>
+                <td><a href="books-by-author.php?id=<?php echo $author['author_id']; ?>">View Books</a></td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</div>
+
+<?php include "view-footer.php"; ?>
