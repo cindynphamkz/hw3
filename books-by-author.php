60220063daf57f48a01d7b05f996c3a318cd9fba
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 01:23:10 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -3,30 +3,21 @@ require_once("model-books.php");
 $pageTitle = "Books by Author";
 include "view-header.php";
 
-$authorId = $_GET['id'];
+// Assume author ID is passed as a query parameter, e.g., books-by-author.php?id=1
+$authorId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 $books = selectBooksByAuthor($authorId);
+
+if ($books->num_rows > 0) {
+    echo "<h1>Books by Author</h1>";
+    echo "<table class='table'>";
+    echo "<thead><tr><th>Title</th><th>Genre</th></tr></thead><tbody>";
+    while ($book = $books->fetch_assoc()) {
+        echo "<tr><td>" . htmlspecialchars($book['Title']) . "</td><td>" . htmlspecialchars($book['Genre']) . "</td></tr>";
+    }
+    echo "</tbody></table>";
+} else {
+    echo "<p>No books found for this author.</p>";
+}
+
+include "view-footer.php";
 ?>
-
-<h1>Books by Author</h1>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Genre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($book = $books->fetch_assoc()): ?>
-            <tr>
-                <td><?php echo $book['bookid']; ?></td>
-                <td><?php echo htmlspecialchars($book['title']); ?></td>
-                <td><?php echo htmlspecialchars($book['genre']); ?></td>
-            </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
-</div>
-
-<?php include "view-footer.php"; ?>
