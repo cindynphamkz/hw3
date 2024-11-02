091f99ae66209663fb9d12319abb6ed7aeeb3327
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 03:02:55 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -4,24 +4,11 @@ $pageTitle = "Books by Author";
 include "view-header.php";
 
 $authorId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+$authorName = "Author Name"; // Replace with a function or query to retrieve the actual author name
 $books = selectBooksByAuthor($authorId);
 
-if ($books && $books->num_rows > 0) {
-    echo "<h1>Books by Author</h1>";
-    echo "<table class='table'>";
-    echo "<thead><tr><th>Title</th><th>Genre</th></tr></thead><tbody>";
-
-    while ($book = $books->fetch_assoc()) {
-        // Use the correct lowercase keys
-        $title = isset($book['title']) ? htmlspecialchars($book['title']) : 'N/A';
-        $genre = isset($book['genre']) ? htmlspecialchars($book['genre']) : 'N/A';
-
-        echo "<tr><td>" . $title . "</td><td>" . $genre . "</td></tr>";
-    }
-    echo "</tbody></table>";
-} else {
-    echo "<p>No books found for this author.</p>";
-}
+// Include the view file for displaying books by author
+include "view-books-by-author.php";
 
 include "view-footer.php";
 ?>
