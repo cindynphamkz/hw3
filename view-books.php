b379ce8fc8530fef0e1baaa01d367bfbc376afb6
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-11-02 03:12:45 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -1,22 +1,26 @@
-<!-- view-books.php -->
-<h1>Books List</h1>
-<table class="table">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Author ID</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($books as $book): ?>
-        <tr>
-            <td><?php echo $book['book_id']; ?></td>
-            <td><?php echo htmlspecialchars($book['title']); ?></td>
-            <td><?php echo htmlspecialchars($book['genre']); ?></td>
-            <td><?php echo $book['author_id']; ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="container mt-5">
+    <h1>Books</h1>
+
+    <?php if ($books && $books->num_rows > 0): ?>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Author ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($book = $books->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($book['Title']); ?></td>
+                        <td><?php echo htmlspecialchars($book['Genre']); ?></td>
+                        <td><?php echo htmlspecialchars($book['AuthorID']); ?></td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No books found for this genre.</p>
+    <?php endif; ?>
+</div>
